8458b6dfffbb26b8b1e092e6a40dfc5dd334eec4
waldeinseinkeit <40184634+siqini@users.noreply.github.com>
2018-07-19 17:04:22 -0500

    Add existing files

--- learn_trees.C
+++ learn_trees.C
@@ -4,3 +4,12 @@
 #include <vector>
 #include <typeinfo>
 #include <cstdlib>
+
+void learn_trees(){
+  /*
+  open a root file, extract the TTree, and print out all its branches
+  */
+  TFile *output=new TFile("output_ExampleAnalysis_ExampleSelection.root");
+  TTree *mytree=(TTree*)output->Get("sbnana");
+  mytree->Print();
+}
