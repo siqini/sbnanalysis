080fcceefc8a7c83b143e2d8fc4530be6fb3e106
waldeinseinkeit <40184634+siqini@users.noreply.github.com>
2018-07-23 11:22:27 -0500

    Add existing files

--- learn_trees.C
+++ learn_trees.C
@@ -10,16 +10,46 @@ void learn_trees(){
   /*
   open a root file, extract the TTree, and print out all its branches
   */
-  TFile *output=new TFile("output_ExampleAnalysis_ExampleSelection.root");
-  TTree *mytree=(TTree*)output->Get("sbnana");
-  //mytree->Print();
+  TFile *myFile=new TFile("output_ExampleAnalysis_ExampleSelection.root");
+  TTree *myTree=(TTree*)myFile->Get("sbnana");
+  myTree->Print();
 
+  auto genieintcode = myTree->GetBranch("interactions.neutrino.genie_intcode");
+  auto nu_energies = myTree->GetBranch("interactions.neutrino.energy");
+  auto nevent = myTree->GetEntries();
+  Event *event = new Event();
+  genieintcode -> SetAddress(&event);
+
+  //find min and max of energies
+  auto E_min=10;
+  auto E_max=0;
+  for (int =0;i<nevent;i++){
+    auto this_energy = nu_energies->GetEntry(i);
+    if (this_energy < E_min){
+      E_min=this_energy;
+    }
+    if (this_energy > E_max){
+      E_max=this_energy;
+    }
+  }
+
+  std::cout<<"number of events: "<<nevent<<std::endl;
+  std::cout<<"E_min: "<<E_min<<std::endl;
+  std::cout<<"E_max: "<<E_max<<std::endl;
+
+
+
+
+
+
+  //mytree->Print();
+/*
   TLeaf* nu_energy=mytree->GetLeaf("interactions.neutrino.energy");
   TLeaf* nu_type=mytree->GetLeaf("interactions.neutrino.genie_intcode");
   auto energy_min=0;
   auto energy_max=10;
   auto nentries=nu_energy->GetBranch()->GetEntries();
-
+  auto ntypes=nu_type->GetBranch()->GetEntries();
   for (int i=0;i<nentries;i++){
     nu_energy->GetBranch()->GetEntry(i);
     auto energy=nu_energy->GetValue();
@@ -29,8 +59,20 @@ void learn_trees(){
   std::cout<<"# energy entries:"<<" "<<nentries<<std::endl;
   std::cout<<"energy min: "<<energy_min<<std::endl;
   std::cout<<"energy max: "<<energy_max<<std::endl;
+*/
+
+/*
+  THStack* parents=new THStack("parents", "parent particles");
+  for (int i=0;i<ntypes;i++){
+    nu_energy->GetBranch()->GetEntry(i);
+    nu_type->GetBranch()->GetEntry(i);
+    auto energy=nu_energy->GetValue();
+    auto type=nu_type->GetValue();}
+*/
+
   /*
   Read a TTree using TTreeReader
-  */
+
   TTreeReader theReader("T",output);
+  */
 }
