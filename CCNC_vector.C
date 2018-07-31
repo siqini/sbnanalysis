f24504722b58dec323f226b3962e6844a7009dcf
waldeinseinkeit <40184634+siqini@users.noreply.github.com>
2018-07-31 16:47:08 -0500

    Add existing files

--- CCNC_vector.C
+++ CCNC_vector.C
@@ -96,8 +96,8 @@ void CCNC_vector(){
   //gPad->BuildLegend(0.75,0.75,0.95,0.95,"");
   auto legend = new TLegend (0.75,0.75,0.95,0.95);
   legend->SetHeader("CC/NC interaction types", "C");
-  legend->AddEntry(h0,"CC->#mu + X","f");
-  legend->AddEntry(h1,"NC->#pi^{#pm} + X","f");
+  legend->AddEntry("h0","CC->#mu + X","f");
+  legend->AddEntry("h1","NC->#pi^{#pm} + X","f");
   legend->Draw();
 
 
