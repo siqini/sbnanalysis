9fd00ee62a9192adc1a9fa763070da841de0ad74
waldeinseinkeit <40184634+siqini@users.noreply.github.com>
2018-07-31 13:39:37 -0500

    Add existing files

--- CCNC_vector.C
+++ CCNC_vector.C
@@ -13,28 +13,65 @@ void CCNC_vector(){
   }
   TTree* myTree = (TTree*) myFile->Get("sbnana");
 
+
   //set up branches
   Event* event = new Event;
   std::vector<int>* CCNC = new std::vector<int>;
   myTree->SetBranchAddress("events", &event);
   myTree->SetBranchAddress("CCNC",&CCNC);
 
+  //vector to store ccnc type info
+  std::vector<int> CCNC_vec;
+  std::vector<double> nu_energy_vec;
+
   for (long i=0;i<myTree->GetEntries();i++){
     myTree->GetEntry(i);
     assert(event->interactions.size() == CCNC->size());
 
-    std::cout<<"Event"<<i<<": ";
+    //std::cout<<"Event"<<i<<": ";
 
     for (size_t j=0;j<event->interactions.size();j++){
       auto enu = event->interactions[j].neutrino.energy;
       auto ccnc_type = CCNC->at(j);
 
-      std::cout<<"Neutrino "<<j<<": "
-               <<"E="<<enu<<" GeV"
-               <<"CCNC type ="<<ccnc_type;
+      CCNC_vec.push_back(ccnc_type);
+      nu_energy_vec.push_back(enu);
+
+      //std::cout<<"Neutrino "<<j<<": "
+              // <<"E="<<enu<<" GeV"
+            //   <<"CCNC type ="<<ccnc_type;
     }
-    std::cout<<std::endl;
+  //  std::cout<<std::endl;
   }
+
+  assert (CCNC_vec.size()==nu_energy_vec.size());
+  THStack* interaction_types = new THStack("interaction_types", "Interaction types");
+  std::vector<TH1D*> hists = std::vector<TH1D*>();
+
+  //set fill colors
+  srand(123);
+  char buffer[10];
+  for (int iHist=0;iHist<2;iHist++){
+    sprintf(buffer,"%s%d","CCNC type",iHist);
+    hists.pushback(new TH1D(buffer, "", 100, 0,10));
+  }
+  hists[0]->SetFillColor(kViolet);
+  hists[1]->SetFillColor(kGreen);
+
+  for (int i=0;i<CCNC_vec.size();i++){
+    auto this_ccnc_type = CCNC_vec[i];
+    auto this_nu_energy = nu_energy_vec[i];
+
+    hists[this_ccnc_type]->Fill(this_nu_energy);
+  }
+
+  for (int iHist=0;iHist<2;iHist++){
+    interaction_types->Add(hists[iHist]);
+  }
+
+  interaction_types->Draw();
+
+
 
 
 }
